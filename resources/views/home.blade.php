942e769258325c6c463a4e3e47baaacc25c7bac5
Marryam Mouslimany <92210054@students.liu.edu.lb>
2024-12-25 12:19:17 +0200

    nav1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,5 @@
-<!doctype html>
-<html lang="en">
-  <head>
+@extends('layouts.nav1')
+@section('title', 'Login')
+@section('content')
 
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
-<link href="https://fonts.googleapis.com/css2?family=Italiana&display=swap" rel="stylesheet">
-
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title','custom')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="{{ asset('css/nav.css') }}" rel="stylesheet">
-
-  </head>
-  
-  <body>
-  <nav class="navbar">
-  <img src="logo.jpeg" alt="Logo">
-  <ul class="nav-links">
-      <li><a href="{{ route('home') }}">Home</a></li>
-      <li><a href="">About Us</a></li>
-      <li><a href="{{ route('login') }}">Login</a></li>
-  </ul>
-</nav>
-@yield('content')
-  </body>
-</html>
+@endsection
--- resources/views/layouts/nav1.blade.php
+++ resources/views/layouts/nav1.blade.php
@@ -1,30 +1,65 @@
-<!doctype html>
+
+
+<!DOCTYPE html>
 <html lang="en">
-  <head>
 
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
-<link href="https://fonts.googleapis.com/css2?family=Italiana&display=swap" rel="stylesheet">
+    <head>
+        <meta charset="utf-8">
+        <title>@yield('title','custom')</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="" name="keywords">
+        <meta content="" name="description">
+
+        <!-- Google Web Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Playball&display=swap" rel="stylesheet">
+
+        <!-- Icon Font Stylesheet -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+        <!-- Libraries Stylesheet -->
+        <link href="{{asset('lib/animate/animate.min.css') }}" rel="stylesheet">
+        <link href="{{asset('lib/lightbox/css/lightbox.min.css') }}" rel="stylesheet">
+        <link href="{{asset('lib/owlcarousel/owl.carousel.min.css') }}" rel="stylesheet">
+
+        <!-- Customized Bootstrap Stylesheet -->
+        <link href="{{asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+        <!-- Template Stylesheet -->
+        <link  href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title','custom')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="{{ asset('css/nav.css') }}" rel="stylesheet">
+            
+       
+    </head>
 
-  </head>
-  
-  <body>
-  <nav class="navbar">
-  <img src="logo.jpeg" alt="Logo">
-  <ul class="nav-links">
-      <li><a href="{{ route('home') }}">Home</a></li>
-      <li><a href="">About Us</a></li>
-      <li><a href="{{ route('login') }}">Login</a></li>
-  </ul>
-</nav>
-@yield('content')
-  </body>
+    <body> <!-- Navbar start -->
+        <div class="container-fluid nav-bar">
+            <div class="container">
+                <nav class="navbar navbar-light navbar-expand-lg py-4">
+                    <a href="index.html" class="navbar-brand">
+                        <h1 class="text-primary fw-bold mb-0">Cater<span class="text-dark">Serv</span> </h1>
+                    </a>
+                    <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                        <span class="fa fa-bars text-primary"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarCollapse">
+                        <div class="navbar-nav mx-auto">
+                            <a href="{{ route('home') }}" class="nav-item nav-link ">Home</a>
+                            <a href="" class="nav-item nav-link ">About Us</a>
+                            <a href="{{ route('login') }}"  class="nav-item nav-link ">Log In</a>
+                          
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <!-- Navbar End -->
+        @yield('content')
+         
+    </body>
 </html>
